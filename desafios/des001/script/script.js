fe5115fd245c8d2c5b3bc8ded88a6246fad732b7
ricardofigueiredocamargo <116184098+ricardofigueiredocamargo@users.noreply.github.com>
2022-12-06 11:20:35 -0300

    atualizando exercício

--- desafios/des001/script/script.js
+++ desafios/des001/script/script.js
@@ -1,17 +1,23 @@
-var corpo = document.body
-var hora = new Date().getHours()
-var minuto = new Date().getMinutes()
-var tempo = document.getElementById('tempo')
-var imagem = document.getElementById('imagem')
+function carregar() {
+    var corpo = document.body
+    var hora = new Date().getHours()
+    var minuto = new Date().getMinutes()
+    var tempo = document.getElementById('tempo')
+    var imagem = document.getElementById('imagem')
 
-tempo.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
-if (hora < 12) {
-    imagem.innerHTML = `<img src="imagens/manha-foto3.jpg" alt="tarde">`
-    corpo.style.background = '#CEBD90'
-} else if (hora <= 18) {
-    imagem.innerHTML = `<img src="imagens/tarde-foto2.jpg" alt="tarde">`
-    corpo.style.background = '#A47762'
-} else {
-    imagem.innerHTML = `<img src="imagens/noite-foto2.jpg" alt="tarde">`
-    corpo.style.background = '#474849'
-} 
+    if (minuto < 10) {
+        minuto = '0' + minuto
+    }
+
+    tempo.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
+    if (hora < 12) {
+        imagem.innerHTML = `<img src="imagens/manha-foto3.jpg" alt="tarde">`
+        corpo.style.background = '#CEBD90'
+    } else if (hora <= 18) {
+        imagem.innerHTML = `<img src="imagens/tarde-foto2.jpg" alt="tarde">`
+        corpo.style.background = '#A47762'
+    } else {
+        imagem.innerHTML = `<img src="imagens/noite-foto2.jpg" alt="tarde">`
+        corpo.style.background = '#474849'
+    }
+}     
