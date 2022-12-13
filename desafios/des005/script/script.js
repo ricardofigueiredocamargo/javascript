088a3811835ef10cd1599e3bc417622eb71e1be9
ricardofigueiredocamargo <116184098+ricardofigueiredocamargo@users.noreply.github.com>
2022-12-13 09:51:01 -0300

    exercício analisador de números

--- desafios/des005/script/script.js
+++ desafios/des005/script/script.js
@@ -4,59 +4,64 @@ let analise = document.getElementById('analise')
 function adicionar() {
     let numero = document.getElementById('inumero')
     let lista = document.getElementById('ilista')
+    let repetido = false
     n = Number(numero.value)
-
-    analise.innerHTML = ''
 
     // para números repetidos
     for (let pos in valores) {
         if (valores[pos] == n) {
-            window.alert('Valor inválido ou já encontrado na lista.')
-            fail
+            repetido = true
         } 
     }
 
-    if (numero.value.length == 0 || n < 1 || n > 100 ) {
+    if (numero.value.length == 0 || n < 1 || n > 100 || repetido == true) {
         window.alert('Valor inválido ou já encontrado na lista.')
     } else {
+        analise.innerHTML = ''
         let item = document.createElement('option')
         item.text = `Valor ${n} adicionado.`
         lista.appendChild(item)
         valores.push(n)
     }  
+
+    numero.value = ''
+    numero.focus()
 }
 
-function analisar() {
-    // total de números
-    if (valores.length == 1) {
-        analise.innerHTML += `<p>Ao todo, temos 1 número cadastrado</p>`
+function finalizar() {
+    analise.innerHTML = ''
+
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
     } else {
-        analise.innerHTML += `<p>Ao todo, temos ${valores.length} números cadastrados</p>`
-    }
-
-    // maior e menor número
-    let maior = valores[0]
-    let menor = valores[0]
-    for (let c = 1; c <= valores.length; c++) {
-        if (maior < valores[c]) {
-            maior = valores[c]
+        // total de números
+        if (valores.length == 1) {
+            analise.innerHTML += `<p>Ao todo, temos 1 número cadastrado</p>`
+        } else {
+            analise.innerHTML += `<p>Ao todo, temos ${valores.length} números cadastrados</p>`
         }
 
-        if (menor > valores[c]) {
-            menor = valores[c]
+        // maior e menor número e soma dos números
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        for (let pos in valores) {
+            if (maior < valores[pos]) {
+                maior = valores[pos]
+            }
+
+            if (menor > valores[pos]) {
+                menor = valores[pos]
+            }
+
+            soma += valores[pos]
         }
-    }
-    analise.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
-    analise.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        analise.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        analise.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        analise.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
 
-    // soma dos números
-    let soma = 0
-    for (let pos in valores) {
-        soma += valores[pos]
+        // média dos números 
+        let media = soma / valores.length
+        analise.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
     }
-    analise.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
-
-    // média dos números 
-    let media = soma / valores.length
-    analise.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
 }
