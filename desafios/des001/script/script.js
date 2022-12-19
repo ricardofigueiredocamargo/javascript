cd908057c7dc3886fb04be108fe8e9514288b5eb
ricardofigueiredocamargo <116184098+ricardofigueiredocamargo@users.noreply.github.com>
2022-12-19 09:45:44 -0300

    desafio javascript

--- desafios/des001/script/script.js
+++ desafios/des001/script/script.js
@@ -1,23 +1,7 @@
-function carregar() {
-    var corpo = document.body
-    var hora = new Date().getHours()
-    var minuto = new Date().getMinutes()
-    var tempo = document.getElementById('tempo')
-    var imagem = document.getElementById('imagem')
-
-    if (minuto < 10) {
-        minuto = '0' + minuto
+function apertar(n) {
+    for (var c = 1; c <= 3; c++) {
+        if (c == n) {
+            window.alert(`Você clicou no ${n}° botão!`)
+        }
     }
-
-    tempo.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
-    if (hora < 12) {
-        imagem.innerHTML = `<img src="imagens/manha-foto3.jpg" alt="tarde">`
-        corpo.style.background = '#CEBD90'
-    } else if (hora <= 18) {
-        imagem.innerHTML = `<img src="imagens/tarde-foto2.jpg" alt="tarde">`
-        corpo.style.background = '#A47762'
-    } else {
-        imagem.innerHTML = `<img src="imagens/noite-foto2.jpg" alt="tarde">`
-        corpo.style.background = '#474849'
-    }
-}     
+}
