985688d3d42f76b6d80460c50a6930dcc491b394
ricardofigueiredocamargo <116184098+ricardofigueiredocamargo@users.noreply.github.com>
2022-12-22 08:56:54 -0300

    desafios javascript

--- desafios/des010/script/script.js
+++ desafios/des010/script/script.js
@@ -16,16 +16,20 @@ function calcularDelta() {
 
     let delta = (b ** 2) - (4 * a * c) 
 
-    resultado.innerHTML = ''
-    resultado.innerHTML += `<h2>Resolvendo Bhaskara</h2>`
-    resultado.innerHTML += `<p>A equação atual é <strong>${a}x² + ${b}x + ${c} = 0</strong></p>`
-    resultado.innerHTML += `<p>O cálculo realizado será <strong>&Delta; = ${b}² - 4 . ${a} . ${c}</strong></p>`
-    resultado.innerHTML += `<p>O valor calculado foi <strong>&Delta; = ${delta}</strong></p>`
+    if (a == 0 || b == 0 || c == 0) {
+        window.alert('Impossível calcular! Tente outros números.')
+    } else {
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<h2>Resolvendo Bhaskara</h2>`
+        resultado.innerHTML += `<p>A equação atual é <strong>${a}x² + ${b}x + ${c} = 0</strong></p>`
+        resultado.innerHTML += `<p>O cálculo realizado será <strong>&Delta; = ${b}² - 4 . ${a} . ${c}</strong></p>`
+        resultado.innerHTML += `<p>O valor calculado foi <strong>&Delta; = ${delta}</strong></p>`
 
-    valores.d = delta
-    segundoCalculo.style.display = 'block'
-    segundoResultado.style.display = 'block'
-    segundoResultado.innerHTML = `<p>Considerando uma equação no formato <em><strong>-b +- &radic;&Delta; / 2 . a</strong></em>, aperte o botão acima para calcular o valor de x.</p>`
+        valores.d = delta
+        segundoCalculo.style.display = 'block'
+        segundoResultado.style.display = 'block'
+        segundoResultado.innerHTML = `<p>Considerando uma equação no formato <em><strong>-b +- &radic;&Delta; / 2 . a</strong></em>, aperte o botão acima para calcular o valor de x.</p>`
+    }
 }
 
 function calcularX() {
@@ -34,12 +38,12 @@ function calcularX() {
     let delta = valores.d
 
     b = -b
-    let positivo = (b + Math.sqrt(delta)) / 2 * a 
-    let negativo = (b - Math.sqrt(delta)) / 2 * a
+    let positivo = (b + Math.sqrt(delta)) / (2 * a) 
+    let negativo = (b - Math.sqrt(delta)) / (2 * a)
 
     segundoResultado.innerHTML = ''
     segundoResultado.innerHTML += `<h2>Resolvendo Bhaskara</h2>`
     segundoResultado.innerHTML += `<p>O cálculo realizado será <strong>${b} +- &radic;${delta} / 2 . ${a}</strong></p>`
-    segundoResultado.innerHTML += `<p>O primeiro valor calculado foi <strong>x1 = ${positivo.toFixed(5)}</strong></p>`
-    segundoResultado.innerHTML += `<p>O segundo valor calculado foi <strong>x2 = ${negativo.toFixed(5)}</strong></p>`
+    segundoResultado.innerHTML += `<p>O primeiro valor calculado foi <strong>x1 = ${positivo.toFixed(2)}</strong></p>`
+    segundoResultado.innerHTML += `<p>O segundo valor calculado foi <strong>x2 = ${negativo.toFixed(2)}</strong></p>`
 }
